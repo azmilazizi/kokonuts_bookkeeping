39e3ec394a056a6e7d92ec5f32fc3978c5d6c004
azmilazizi <azmilazizi1999@graduate.utm.my>
2025-12-22 01:43:34 +0800

    build latest

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "652307187"
+    serviceWorkerVersion: "3427342888"
   }
 });
--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "6eb416dd1b7b106d594f7fc73cc964ee",
+const RESOURCES = {"flutter_bootstrap.js": "97c25df4d66c7c01ad29e2dd24b36792",
 "version.json": "238747f89919f9d40a20959bc47888fc",
 "index.html": "ba86a0fb98e7b832524ee509a0f0eb60",
 "/": "ba86a0fb98e7b832524ee509a0f0eb60",
-"main.dart.js": "8cddc2af693c27d821a4242da9a3ec84",
+"main.dart.js": "40b05f91f6bc54fe5f31b57eda436c56",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "123345d1654f3da15915ebf7d748baf9",
 "icons/app_logo_light.png": "05c447358c3021c896735b5aec1fd14a",
@@ -28,7 +28,7 @@ const RESOURCES = {"flutter_bootstrap.js": "6eb416dd1b7b106d594f7fc73cc964ee",
 "assets/packages/syncfusion_flutter_pdfviewer/assets/underline.png": "c94a4441e753e4744e2857f0c4359bf0",
 "assets/shaders/ink_sparkle.frag": "ecc85a2e95f5e9f53123dcaf8cb9b6ce",
 "assets/AssetManifest.bin": "06caf87ebd9f7faa192c41c239fad422",
-"assets/fonts/MaterialIcons-Regular.otf": "d2f4c07b9bf50c069e12256fe8f1cce6",
+"assets/fonts/MaterialIcons-Regular.otf": "9994ee8cd5b179760c95a079e4129adf",
 "assets/assets/images/app_logo_light.png": "05c447358c3021c896735b5aec1fd14a",
 "assets/assets/images/app_logo_dark.png": "c1265626e3b78c01ca7cc970de165492",
 "canvaskit/skwasm.js": "1ef3ea3a0fec4569e5d531da25f34095",
