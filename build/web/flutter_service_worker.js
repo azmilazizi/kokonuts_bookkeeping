e40c788be51fdeffef9664e712f2a53b1a7b22ad
azmilazizi <azmilazizi1999@graduate.utm.my>
2026-03-06 19:40:18 +0800

    latest build

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "d228ae26ff2a97bacb38fe3002d211fc",
+const RESOURCES = {"flutter_bootstrap.js": "d77bed273b348e7eebde468316887614",
 "version.json": "238747f89919f9d40a20959bc47888fc",
 "index.html": "3089070c17ee6acbf8706d4fbea8b3c6",
 "/": "3089070c17ee6acbf8706d4fbea8b3c6",
-"main.dart.js": "e7e67ffc91343e0b28f943c05e5d224a",
+"main.dart.js": "c7a2b4f7354d818f12f0364aa5f5a449",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "64f8f784da39a0cf632e22baf3f1b977",
 "icons/app_logo_light.png": "3d54fcbd0fe3ce71774fcce83858858a",
